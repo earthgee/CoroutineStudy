11c32050012e77419cd9054ce4970a0e05cd9ce7
earthgee <865603556@qq.com>
2024-03-25 09:38:22 +0800

    move lite

--- src/main/kotlin/coroutineLite/Delay.kt
+++ src/main/kotlin/coroutineLite/Delay.kt
@@ -1,14 +1,16 @@
+package coroutineLite
+
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
-suspend fun Delay(time:Long, unit: TimeUnit = TimeUnit.MILLISECONDS) {
+suspend fun edelay(time:Long, unit: TimeUnit = TimeUnit.MILLISECONDS) {
     if(time <= 0){
         return
     }
 
-    suspendCoroutine<Unit> {  continuation ->
+    suspendCoroutine<Unit> { continuation ->
         executor.schedule({
             continuation.resume(Unit)
         }, time, unit)
